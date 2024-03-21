ec0760204b61603fd5515ad0e89cb802a3640be9
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-03-22 01:37:32 +0200

    update"add alert component"

---
+++ src/components/ui/Alert/Alert.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from "react"
+import "./index.scss"
+import { X } from "lucide-react"
+
+interface IProps {
+type : string;
+title: string;
+icon:ReactNode;
+description:string;
+}
+const Alert: FC<IProps> = ({type="alert-danger",title,icon,description}) => {
+  return (
+    <div className={type}>
+      <div className="alert-header">
+        <div className="title">
+
+        <span>{icon}</span>
+          <h4>{title}</h4>
+        </div>
+        <X  className="close" size={20}/>
+      </div>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+export default Alert
