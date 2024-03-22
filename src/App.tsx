d7a585e7a73803b076ccf1d69e07c6ad078b9b75
Mohamed Elsayed <122582599+MoElsayed90@users.noreply.github.com>
2024-03-22 03:02:31 +0200

    update"add children to alert compenent"

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ function App() {
   return (
     <>
       <div >
-        <Alert type={"alert-error"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.      Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<Ban size={20} />} />
-        <Alert type={"alert-success"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.      Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<CheckCheck size={20} />} />
-        <Alert type={"alert-info"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.      Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<Info size={20} />} />
-        <Alert type={"alert-warning"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.      Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<AlertTriangle size={20} />} />
-        <Alert type={"alert-default"} title={"Upgrade your plan"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.      Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<Bell size={20} />} />
+        <Alert type={"alert-error"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<Ban size={20} />} />
+        <Alert type={"alert-info"} title={"Something went wrong"} icon={<Info size={20} />}>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium corrupti beatae recusandae qui officiis itaque quisquam libero sunt neque illo adipisci,<a href="/">excepturi ducimus magni,</a>  necessitatibus assumenda eaque magnam. Saepe, corrupti!
+          </p>
+        </Alert>
+        <Alert type={"alert-success"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<CheckCheck size={20} />} />
+        <Alert type={"alert-warning"} title={"Something went wrong"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<AlertTriangle size={20} />} />
+        <Alert type={"alert-default"} title={"Upgrade your plan"} description="Lorem ipsum dolor sit amet consectetur, adipisicing elit.Nihil, corporis. Totam qui facilis praesentium quis, sunt impedit architecto hic et?" icon={<Bell size={20} />} />
       </div>
     </>
   )
--- src/components/ui/Alert/Alert.tsx
+++ src/components/ui/Alert/Alert.tsx
@@ -1,25 +1,29 @@
 import { FC, ReactNode } from "react"
 import "./index.scss"
 import { X } from "lucide-react"
+import { AlertTypes } from "../../../types";
 
 interface IProps {
-type : string;
-title: string;
-icon:ReactNode;
-description:string;
+  type: AlertTypes;
+  title: string;
+  icon: ReactNode;
+  description?: string;
+  children?:ReactNode;
 }
-const Alert: FC<IProps> = ({type,title,icon,description}) => {
+const Alert: FC<IProps> = ({ type, title, icon, description, children}) => {
   return (
     <div className={type}>
       <div className="alert-header">
         <div className="title">
+          
 
-        <span>{icon}</span>
+          <span>{icon}</span>
           <h4>{title}</h4>
         </div>
-        <X  className="close" size={20}/>
+        <X className="close" size={20} />
       </div>
-      <p>{description}</p>
+      
+      {children ? children  :<p>{description}</p> }
     </div>
   )
 }
